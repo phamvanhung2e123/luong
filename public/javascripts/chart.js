2ecc3e8ed22929824c97bdb6e0313f95cf6aaf76
System Administrator <root@phamvanhung.local>
2014-03-29 02:17:28 +0900

    fix

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -81,6 +81,7 @@ $(function() {
         console.log("Current :"+ current_all_global);
         rg.update(current_all);
     });
+
     var current_all_paid_global = 0;
 
 
@@ -250,6 +251,108 @@ $(function() {
             ]
         });
     }
+
+    function initialize() {
+        var mapOptions = {
+            zoom: 10,
+            center: new google.maps.LatLng(-33.9, 151.2)
+        }
+        var map = new google.maps.Map(document.getElementById('map'),
+            mapOptions);
+
+        setMarkers(map, beaches);
+    }
+
+    /**
+     * Data for the markers consisting of a name, a LatLng and a zIndex for
+     * the order in which these markers should display on top of each
+     * other.
+     */
+    var beaches = [
+        ['Bondi Beach', -33.890542, 151.274856, 4],
+        ['Coogee Beach', -33.923036, 151.259052, 5],
+        ['Cronulla Beach', -34.028249, 151.157507, 3],
+        ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+        ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
+
+    function setMarkers(map, locations) {
+        // Add markers to the map
+
+        // Marker sizes are expressed as a Size of X,Y
+        // where the origin of the image (0,0) is located
+        // in the top left of the image.
+
+        // Origins, anchor positions and coordinates of the marker
+        // increase in the X direction to the right and in
+        // the Y direction down.
+        var image = {
+            url: 'css/icon.png'
+        };
+        // Shapes define the clickable region of the icon.
+        // The type defines an HTML &lt;area&gt; element 'poly' which
+        // traces out a polygon as a series of X,Y points. The final
+        // coordinate closes the poly by connecting to the first
+        // coordinate.
+        var shape = {
+            coord: [1, 1, 1, 20, 18, 20, 18 , 1],
+            type: 'poly'
+        };
+        setInterval(function() {
+            var beach = ['Maroubra Beach' + getRandomInt(1,100), -33.950198 + getRandomInt(1,100)/100, 151.259302 + getRandomInt(1,100)/100, 1];
+            var myLatLng = new google.maps.LatLng(beach[1], beach[2]);
+
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                icon: image,
+                shape: shape,
+                title: beach[0],
+                zIndex: beach[3]
+            });
+        }, 1000);
+
+
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    function getRandomInt (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function buildMap(){
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                var options = {
+                    zoom: 15,
+                    center: latlng,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+                var map = new google.maps.Map(document.getElementById('map'), options);
+                var image = new google.maps.MarkerImage('http://www.preschool-navi.jp/static/img/search/icon.png',
+                    // This marker is 20 pixels wide by 32 pixels tall.
+                    // The origin for this image is 0,0.
+                    new google.maps.Point(0,0),
+                    // The anchor for this image is the base of the flagpole at 0,32.
+                    new google.maps.Point(0, 32));
+                var marker = new google.maps.Marker({
+                    position: latlng,
+                    map: map,
+                    title: '現在地',
+                    icon: image
+                });
+                alert(marker);
+            }, function(e) {
+                document.getElementById('message').innerHTML = typeof e == 'string' ? e : e.message;
+            });
+        } else {
+            document.getElementById('message').innerHTML = 'Location APIがサポートされていません。';
+        }
+    }
     buildStockGraph();
+
+
 
 });
